139f151eb498b59c2c2e3c5f85826c8a0c05c7ba
aprilsblue <jg0428.lim@gmail.com>
2020-12-30 12:03:02 +0900

    Set Oracle EL Price (#2)
    * set oracle EL price
    
    * update getElPrice
    
    Co-authored-by: 임정건 <aprilsblue@imjeong-geon-ui-MacBookPro.local>

--- contracts/PriceManager.sol
+++ contracts/PriceManager.sol
@@ -3,59 +3,86 @@ pragma solidity 0.7.0;
 
 import "./EAccessControl.sol";
 
+interface OraclePrice {
+	function getCurrentPrice() external view returns (uint256);
+}
+
 /**
  * @title PriceManager
  * @notice Manage elysia price and asset token price
  * @author Elysia
  */
 contract PriceManager is EAccessControl {
-    /// @notice Emitted when el Price is changed
-    event NewElPrice(uint256 newElPrice);
+	/// @notice Emitted when el Price is changed
+	event NewElPrice(uint256 newElPrice);
 
-    /// @notice Emitted when price is changed
-    event NewPrice(uint256 newPrice);
+	/// @notice Emitted when price is changed
+	event NewPrice(uint256 newPrice);
 
-    // USD per Elysia token
-    // decimals: 18
-    uint256 public _elPrice;
+	/// @notice Emitted when price contract address is changed
+	// event NewPriceContractAddress(address priceContractAddress);
+	event NewSetPriceContract(address priceContractAddress);
 
-    // USD per Elysia Asset Token
-    // decimals: 18
-    uint256 public _price;
+	// USD per Elysia token
+	// decimals: 18
+	uint256 public _elPrice;
 
-    // TODO
-    // Use oracle like chainlink
-    function getElPrice() public view returns (uint256) {
-        return _elPrice;
-    }
+	// USD per Elysia Asset Token
+	// decimals: 18
+	uint256 public _price;
 
-    function getPrice() public view returns (uint256) {
-        return _price;
-    }
+	OraclePrice public oracle_price;
 
-    function setElPrice(uint256 elPrice_) external onlyAdmin returns (bool) {
-        _elPrice = elPrice_;
+	// TODO
+	// Use oracle like chainlink
+	function getElPrice() public view returns (uint256) {
+		if(address(oracle_price) != address(0)) {
+			uint256 _elOraclePrice = oracle_price.getCurrentPrice();
+			return _elOraclePrice;
+		} else {
+			return _elPrice;
+		}
+	}
 
-        emit NewElPrice(elPrice_);
+	function getPrice() public view returns (uint256) {
+		return _price;
+	}
 
-        return true;
-    }
+	function setElPrice(uint256 elPrice_) external onlyAdmin returns (bool) {
+		_elPrice = elPrice_;
 
-    function setPrice(uint256 price_) external onlyAdmin returns (bool) {
-        _price = price_;
+		emit NewElPrice(elPrice_);
 
-        emit NewPrice(price_);
+		return true;
+	}
 
-        return true;
-    }
+	function setPrice(uint256 price_) external onlyAdmin returns (bool) {
+		_price = price_;
 
-    function toElAmount(uint256 amount) public view returns (uint256) {
-        uint256 amountEl = (amount * _price * (10**18)) / _elPrice;
-        require(
-            (amountEl / amount) == ((_price * (10**18)) / _elPrice),
-            "PriceManager: multiplication overflow"
-        );
+		emit NewPrice(price_);
 
-        return amountEl;
-    }
+		return true;
+	}
+
+	function toElAmount(uint256 amount) public view returns (uint256) {
+		uint256 amountEl = (amount * _price * (10**18)) / _elPrice;
+		require(
+			(amountEl / amount) == ((_price * (10**18)) / _elPrice),
+			"PriceManager: multiplication overflow"
+		);
+
+		return amountEl;
+	}
+
+	function setPriceContract(address priceContractAddress_) external onlyAdmin returns (bool) {
+		oracle_price = OraclePrice(priceContractAddress_);
+
+		emit NewSetPriceContract(priceContractAddress_);
+
+		return true;
+	}
+
+	function getOracleContract() external view returns (address) {
+		return address(oracle_price);
+	}
 }
