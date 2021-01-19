b6b62314985dc6fcb0147604a78d85590992c081
jbzang2000 <55565307+jbzang2000@users.noreply.github.com>
2021-01-20 02:50:14 +0900

    testnet deploy setting

--- scripts/deployArguments/AssetTokenEth.js
+++ scripts/deployArguments/AssetTokenEth.js
@@ -5,11 +5,11 @@ const interestRate = '1' + '0'.repeat(17);
 
 module.exports = {
     //el_: "0x5735Af25c6BE1b5822ccd03CDaD3C84eB1e0E9C8", //Eth는 EL 주소 필요 없음
-    eController_: "", //deploy 이후 넣어야 하는 값
+    eController_: "", //아직 eController가 없어요
     amount_: 10000,
     price_: price,
     rewardPerBlock_: rewardPerBlock,
-    payment_: 0,
+    payment_: 1,
     latitude_: 123,
     longitude_: 456,
     assetPrice_: assetPrice,
---
+++ scripts/verifyArguments/AssetTokenELVerify.js
@@ -0,0 +1,20 @@
+const price = '5' + '0'.repeat(18);
+const assetPrice = '5' + '0'.repeat(21);
+const rewardPerBlock = '5' + '0'.repeat(14);
+const interestRate = '1' + '0'.repeat(17);
+
+module.exports = [
+    "0x5735Af25c6BE1b5822ccd03CDaD3C84eB1e0E9C8", // Ropsten EL
+    "0xE893e72cA5C4a7fE52aFfef054C0f33d932027F9", //아직 eController가 없어요
+    10000,
+    price,
+    rewardPerBlock,
+    0,
+    123,
+    456,
+    assetPrice,
+    interestRate,
+    "AssetBlueEL",
+    "ABEL",
+    0
+]
