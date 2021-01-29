2997c4ca7105816cbf6cf59b98b048e7484b22fa
jbzang2000 <55565307+jbzang2000@users.noreply.github.com>
2021-01-29 12:19:56 +0900

    deploy argument fix

--- scripts/deployArguments/AssetTokenEth.ts
+++ scripts/deployArguments/AssetTokenEth.ts
@@ -1,16 +1,16 @@
 const AssetTokenEth = {
   //el_: "0x5735Af25c6BE1b5822ccd03CDaD3C84eB1e0E9C8", //Eth는 EL 주소 필요 없음
-  eController_: "", //아직 eController가 없어요
+  eController_: "",
   amount_: 10000,
   price_: "5" + "0".repeat(18),
-  rewardPerBlock_: "5" + "0".repeat(14),
+  rewardPerBlock_: "895289551784491",
   payment_: 1,
-  latitude_: 123,
-  longitude_: 456,
-  assetPrice_: "5" + "0".repeat(21),
-  interestRate_: "1" + "0".repeat(17),
-  name_: "AssetBlueEth",
-  symbol_: "ABEth",
+  latitude_: 3728493,
+  longitude_: 12656264,
+  assetPrice_: "27150" + "0".repeat(18),
+  interestRate_: "8" + "0".repeat(16),
+  name_: "AssetBlue#3Eth",
+  symbol_: "AB3Eth",
   decimals_: 0,
 };
 
