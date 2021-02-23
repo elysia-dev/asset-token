c0dd670be321f3accfd851ce6933704c8c9d5219
donguk.seo <donguk.seo@modori.io>
2021-02-23 12:25:26 +0900

    Deploy mainnet 1

--- scripts/verifyAssets.ts
+++ scripts/verifyAssets.ts
@@ -3,54 +3,14 @@ import { exec } from "child_process";
 
 // Deployscript Result
 const txResult = `
-controller address: 0xbc0d9B8f91F1cCD28cd6966bf3C7c692DeF23604
-assetTokenBlue2 address: 0xa1e8DAe38a6164d7D8721E98c2dd5B7ba86a16d9
-assetTokenEth address: 0xa212F0d560c90a2C48Cc2b0B5C05ab4EE7007Ab00
+controller address:0x5b1Df4d7fAc33B24a8DEF3499ACAE0347AaB31a7
+assetTokenBlue2 address:0x7Aa1BF2f7e42a94b68f4c4Dfc6761ab096bA387e
+assetTokenBlue3EL address:0xbbe88f80628BA092b833e6B1804Ed7fc97E334D4
+assetTokenBlue3Eth address:0x3206A51f6b129F4E0b23d3064753e58582724aF3
 `;
 
-const [
-  controller,
-  assetTokenBlue2,
-  assetTokenBlue3EL,
-  assetTokenBlue3ETH,
-] = txResult.split('\n').filter((body) => body).map((result) => result.split(':')[1]);
-
-// Verify Controller
-exec(`yarn hardhat verify --network ${hardhat.network.name} ${controller}`, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
-
-exec(`CONTROLLER=${controller} yarn hardhat verify --network ${hardhat.network.name} --constructor-args scripts/verifyArguments/ELAB2.js ${assetTokenBlue2}`, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
-
-exec(`CONTROLLER=${controller} yarn hardhat verify --network ${hardhat.network.name} --constructor-args scripts/verifyArguments/ELAB3EL.js ${assetTokenBlue3EL}`, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
+const controller = '0x5b1Df4d7fAc33B24a8DEF3499ACAE0347AaB31a7'
+const assetTokenBlue3ETH = '0xC4790246bCbE3e9026235307E53aCE5322496557'
 
 exec(`CONTROLLER=${controller} yarn hardhat verify --network ${hardhat.network.name} --constructor-args scripts/verifyArguments/ELAB3ETH.js ${assetTokenBlue3ETH}`, (error, stdout, stderr) => {
   if (error) {
